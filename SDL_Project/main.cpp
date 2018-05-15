f11eee9d59bb18925665c20d7628a9dfd7d65048
FelixS96 <felix.s96ss@gmail.com>
2018-05-15 11:36:14 +0200

    1

--- SDL_Project/main.cpp
+++ SDL_Project/main.cpp
@@ -9,6 +9,30 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
+	SDL_Window* window = SDL_CreateWindow("Test", 50, 50, 640, 480, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED);
+	double i = 0;
+	SDL_Event e;
+	while (true) {
+		while (SDL_PollEvent(&e)) {
+			
+		}
+		
+		i += 0.0001;
+		SDL_SetRenderDrawColor(renderer, (sin(i)+1) / 2 * 255, (sin(i) + 1) / 2 * 255, 0, 255);
+		SDL_RenderClear(renderer);
+
+		SDL_Rect rect;
+		rect.x = 50;
+		rect.y = 50;
+		rect.w = 100;
+		rect.h = 100;
+		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+		SDL_RenderDrawRect(renderer, &rect);
+
+		SDL_RenderPresent(renderer);
+	}
+	SDL_Delay(5000);
 	SDL_Quit();
 	return 0;
 }
